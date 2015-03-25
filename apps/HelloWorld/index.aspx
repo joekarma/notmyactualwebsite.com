1b70913b58f79d1b403fc3d92e4c1b63f0a6227c
Joe Taylor <taylor@c0de.co>
2015-03-25 01:47:34 -0700

    Added hello world junk.

---
+++ apps/HelloWorld/index.aspx
@@ -0,0 +1,5 @@
+<%@ Language="C#" Inherits="HelloWorld.MyPage" src="index.aspx.cs" %>
+
+<%
+	Response.Write("Hello, world.");
+%>
---
+++ apps/HelloWorld/index.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Web;
+using System.Web.UI;
+
+namespace HelloWorld
+{
+    public class MyPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
